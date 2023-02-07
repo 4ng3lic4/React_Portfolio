03ec9484be336a2cb37df6751926ae05387e3647
4ng3lic4 <ositosviajeros@hotmail.com>
2023-02-07 12:48:34 -0500

    add Navbar.jsx file in new scenes folder

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,16 @@
+import Navbar from "./scenes/Navbar";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery"
 
 function App() {
-   //Adding states
-   const [ selectedPage, ]
-  return (
-    <div className="App">
-     
-    </div>
-  );
+   //Adding states, this one is stating what part of the navigation from the website we are at
+   const [ selectedPage, setSelectedPage ] = useState('home');
+   //This variable is storing the size of the screen
+   const isAboveMediumScreens = useMediaQuery("min-width: 1060px")
+
+  return <div className="app bg-deep-blue">
+    <Navbar selectedPage={selectedPage} setSelectedPage={ setSelectedPage}/>
+  </div>
 }
 
 export default App;
